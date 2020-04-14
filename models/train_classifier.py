68215a5f97c02f337500a6232491c39bafcea88d
Lukas <Lukas.Forst48@googlemail.com>
2020-04-14 12:29:35 +0200

    Initial Commit

---
+++ models/train_classifier.py
@@ -0,0 +1,122 @@
+import sys
+from sqlalchemy import create_engine
+import sqlite3
+import re
+import numpy as np
+import pandas as pd
+import pickle
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import nltk
+nltk.download(['punkt', 'wordnet','stopwords', 'averaged_perceptron_tagger'])
+from nltk.corpus import stopwords
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.datasets import make_multilabel_classification
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+def load_data(database_filepath):
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    #conn = sqlite3.connect(database_filepath)
+    #engine = create_engine('sqlite:///{}'.format(database_filepath))
+    
+
+    df = pd.read_sql_table('datatable', engine)
+
+    X= df['message']
+
+    Y= df.iloc[:, 5:]
+
+    #df = pd.read_sql('SELECT * FROM datatable', engine)
+    #X = df['message']
+    #Y = df.drop(['id','message','original','genre'],axis=1)
+
+    return X, Y, df
+
+
+def tokenize(text):
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    
+    stop_words = stopwords.words("english")
+    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
+    return tokens
+
+
+def build_model():
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+          ('tfidf', TfidfTransformer()),
+         ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    parameters = parameters = {
+        'vect__ngram_range': ((1, 1), (1, 2)),
+        
+        #'clf__estimator__n_jobs': [1, 2],
+
+        'clf__estimator__min_samples_split': [2, 3],
+
+        'clf__estimator__criterion': ['entropy', 'gini']
+        
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters,  n_jobs=-1,verbose=2)
+    return cv
+    
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    modpred = model.predict(X_test)
+    for row,col in enumerate(Y_test):
+    
+        target = str(Y_test[col].unique())
+        target = re.sub(r"[\[\]']", "",target).replace(" ", "")
+    
+        print(classification_report(Y_test[col],modpred[ : , row],target_names=target))
+
+
+def save_model(model, model_filepath):
+    
+    with open(model_filepath, 'wb') as file:
+        pickle.dump(model, file)
+    
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
